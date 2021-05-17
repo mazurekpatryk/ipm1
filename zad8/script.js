acdec8fd9d4cd5a65c479cd221b68b77ced3e367
Keruzaimx <patrykmazurek13@gmail.com>
2021-05-17 18:19:43 +0200

    added img to DB and show img

--- zad8/script.js
+++ zad8/script.js
@@ -31,6 +31,13 @@ $(document).ready(function () {
 	};
 });
 
+function createCanvas(addressImg) {
+
+}
+
+
+function sumLetter(txt) { let newtxt = ""; let sum = 0; if(txt != null) for (let i = 0; i < txt.length; ++i) { var value = txt.charAt(i); if(value == 'a'){ sum += 1; }else if(value == 'b'){ sum += 2; }else if(value == 'c'){ sum += 3; }else if(value == 'd'){ sum += 4; }else if(value == 'e'){ sum += 5; }else if(value == 'f'){ sum += 6; }else if(value == 'g'){ sum += 7; }else if(value == 'h'){ sum += 8; }else if(value == 'i'){ sum += 9; }else if(value == 'j'){ sum += 10; }else if(value == 'k'){ sum += 11; }else if(value == 'l'){ sum += 12; }else if(value == 'm'){ sum += 13; }else if(value == 'n'){ sum += 14; }else if(value == 'o'){ sum += 15; }else if(value == 'p'){ sum += 16; }else if(value == 'q'){ sum += 17; }else if(value == 'r'){ sum += 18; }else if(value == 's'){ sum += 19; }else if(value == 't'){ sum += 20; }else if(value == 'u'){ sum += 21; }else if(value == 'v'){ sum += 22; }else if(value == 'w'){ sum += 23; }else if(value == 'x'){ sum += 24; }else if(value == 'y'){ sum += 25; }else if(value == 'z'){ sum += 26; }else if(value == 'A'){ sum += 31; }else if(value == 'B'){ sum += 32; }else if(value == 'C'){ sum += 33; }else if(value == 'D'){ sum += 34; }else if(value == 'E'){ sum += 35; }else if(value == 'F'){ sum += 36; }else if(value == 'G'){ sum += 37; }else if(value == 'H'){ sum += 38; }else if(value == 'I'){ sum += 39; }else if(value == 'J'){ sum += 40; }else if(value == 'K'){ sum += 41; }else if(value == 'L'){ sum += 42; }else if(value == 'M'){ sum += 43; }else if(value == 'N'){ sum += 44; }else if(value == 'O'){ sum += 45; }else if(value == 'P'){ sum += 46; }else if(value == 'Q'){ sum += 47; }else if(value == 'R'){ sum += 48; }else if(value == 'S'){ sum += 49; }else if(value == 'T'){ sum += 50; }else if(value == 'U'){ sum += 51; }else if(value == 'V'){ sum += 52; }else if(value == 'W'){ sum += 53; }else if(value == 'X'){ sum += 54; }else if(value == 'Y'){ sum += 55; }else if(value == 'Z'){ sum += 56; } } return sum; }
+
 
 function addCustomer() {
 	var name = $('#name').val();
@@ -42,11 +49,50 @@ function addCustomer() {
 	var city = $('#city').val();
 	var nipnumber = $('#nipnumber').val();
 	var idnumber = $('#idnumber').val();
+	var addressImg = $('#addressImg').val();
 
 	var transaction = db.transaction(["customers"], "readwrite");
 
 	var store = transaction.objectStore("customers");
 
+
+	let sum = 0;
+    sum += sumLetter(name);
+	sum += sumLetter(email);
+	sum += sumLetter(surname);
+	sum += sumLetter(postcode);
+	sum += sumLetter(phonenumber);
+	sum += sumLetter(address);
+	sum += sumLetter(city);
+	sum += sumLetter(nipnumber);
+	sum += sumLetter(idnumber);
+	sum += sumLetter(addressImg);
+    
+    var colorR = sum % 255;
+    var colorG = 255 - (sum % 255);
+    var colorB = (0.5*colorR>125)?99:199;
+	let rgba_filter = "rgba(" + colorR + ", " + colorG + ", " + colorB + ", 0.5)";
+	
+
+	//canvas
+
+	// var canvas = document.getElementById('raed');
+	// var ctx = canvas.getContext("2d");
+
+	// var img = new Image();
+	// img.src = 'https://jazz.pl/wp-content/uploads/Best-top-desktop-beautiful-nature-wallpapers-hd-nature-wallpaper-picture-image-photo-61.jpg';
+	// ctx.drawImage(img, 10, 10);
+	// var dataURL = canvas.toDataURL("image/jpeg");
+	// console.log(dataURL);
+
+	// //2
+
+	// var canvas = document.getElementById('show');
+	// var ctx = canvas.getContext('2d');
+	// var myImage = new Image();
+	// myImage.src = dataURL;
+	// ctx.drawImage(myImage, 0, 0);
+	//////
 
 	var customer = {
 		name: name,
@@ -57,7 +103,9 @@ function addCustomer() {
 		address: address,
 		city: city,
 		nipnumber: nipnumber,
-		idnumber: idnumber
+		idnumber: idnumber,
+		addressImg: addressImg,
+		rgba_filter: rgba_filter
 	};
 
 	var request = store.add(customer);
@@ -95,6 +143,8 @@ function showCustomers(e) {
 
 			output += "<td><span class='cursor customer' contenteditable='true' data-field='postcode' data-id='" + cursor.value.id + "'>" + cursor.value.postcode + "</span></td>";
 			output += "<td><span class='cursor customer' contenteditable='true' data-field='city' data-id='" + cursor.value.id + "'>" + cursor.value.city + "</span></td>";
+			//output += "<td><span class='cursor customer' contenteditable='true' data-field='city' data-id='" + cursor.value.id + "'>" + cursor.value.rgba_filter + "</span></td>";
+			output += '<td><div id="image"' + 'style="background-color: '+cursor.value.rgba_filter+';"><img src=' + cursor.value.addressImg +'" alt="" style="width:100px;height:100px; opacity: 0.5;">' + "</td>";
 			output += "<td><a onclick='removeCustomer(" + cursor.value.id + ")' href=''>Delete</a></td>";
 			output += "</tr>";
 			cursor.continue();
@@ -220,7 +270,7 @@ function makeid(length) {
 }
 
 function makeText(txt) {
-	let random = txt[Math.floor(Math.random() * (txt.length-1))];
+	let random = txt[Math.floor(Math.random() * (txt.length - 1))];
 	return random;
 }
 
@@ -282,4 +332,124 @@ if (input != null) {
 
 
 
+function workerFunctionReadLink() {
 
+	let w;
+	let colorR = 0;
+	let colorG = 0;
+	let colorB = 0;
+
+	document.getElementById('addImage').onclick = function (e) {
+		let addressImg = $('#addressImg').val();
+		colorR = 0;
+		colorG = 0;
+		colorB = 0;
+		let name = $('#name').val();
+		var email = $('#email').val();
+		var surname = $('#surname').val();
+		var postcode = $('#postcode').val();
+		var phonenumber = $('#phonenumber').val();
+		var address = $('#address').val();
+		var city = $('#city').val();
+		var nipnumber = $('#nipnumber').val();
+		var idnumber = $('#idnumber').val();
+
+// var canvas = document.getElementById('raed');
+				// var ctx = canvas.getContext("2d");
+
+				// var img = new Image();
+				// img.src = addressImg;
+				// ctx.drawImage(img, 0, 0);
+				// var dataURL = canvas.toDataURL("image/jpeg");
+				// console.log(dataURL);
+
+
+		let txt = '{"addressImg": "' + addressImg + '", "colorR": "' + colorR + '", "colorG": "' + colorG +
+			'", "colorB": "' + colorB + '", "name": "' + name + '", "surname": "' + surname +
+			'", "email": "' + email +
+			'", "postcode": "' + postcode + '", "phonenumber": "' + phonenumber + '", "address": "' +
+			address +
+			'", "city": "' + city + '", "nipnumber": "' + nipnumber + '", "idnumber": "' + idnumber + '"}';
+		let objJson = JSON.parse(txt);
+		console.log(objJson);
+
+
+		if (typeof (Worker) !== "undefined") {
+			if (typeof (w) == "undefined") {
+				w = new Worker("workers_filter.js");
+			} else {
+				console.log("Tuuu!");
+			}
+
+			w.addEventListener('message', function (e) {
+				colorR = e.data["colorR"];
+				colorG = e.data["colorG"];
+				colorB = e.data["colorB"];
+				document.getElementById("filter").style.backgroundColor = "rgba(" + colorR + ", " +
+					colorG + ", " + colorB + ", 0.5)";
+				let url = "url('" + addressImg + "')";
+				document.getElementById("image").style.backgroundImage = url;
+
+
+			}, false);
+			w.postMessage(objJson);
+
+		} else {
+			console.log("Sorry, your browser does not support Web Workers...");
+		}
+
+
+	}
+
+}
+
+
+function workerFunction() {
+
+	let w;
+
+	document.getElementById('change').onclick = function (e) {
+		let name = $('#name').val();
+		var email = $('#email').val();
+		var surname = $('#surname').val();
+		var postcode = $('#postcode').val();
+		var phonenumber = $('#phonenumber').val();
+		var address = $('#address').val();
+		var city = $('#city').val();
+		var nipnumber = $('#nipnumber').val();
+		var idnumber = $('#idnumber').val();
+
+		let txt = '{"name": "' + name + '", "surname": "' + surname + '", "email": "' + email +
+			'", "postcode": "' + postcode + '", "phonenumber": "' + phonenumber + '", "address": "' +
+			address +
+			'", "city": "' + city + '", "nipnumber": "' + nipnumber + '", "idnumber": "' + idnumber + '"}';
+		let objJson = JSON.parse(txt);
+		console.log(objJson);
+
+		///
+		if (typeof (Worker) !== "undefined") {
+			if (typeof (w) == "undefined") {
+				w = new Worker("demo_workers.js");
+			} else {
+				console.log("Tuuu!");
+			}
+
+			w.addEventListener('message', function (e) {
+				$('#name').val(e.data["name"]);
+				$('#email').val(e.data["email"]);
+				$('#surname').val(e.data["surname"]);
+				$('#postcode').val(e.data["postcode"]);
+				$('#phonenumber').val(e.data["phonenumber"]);
+				$('#address').val(e.data["address"]);
+				$('#city').val(e.data["city"]);
+				$('#nipnumber').val(e.data["nipnumber"]);
+				$('#idnumber').val(e.data["idnumber"]);
+			}, false);
+			w.postMessage(objJson);
+
+		} else {
+			console.log("Sorry, your browser does not support Web Workers...");
+		}
+	}
+
+}
