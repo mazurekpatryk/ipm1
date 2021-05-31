e0a05bcdcacec57cd4cc6b257794d166964345de
Keruzaimx <patrykmazurek13@gmail.com>
2021-05-31 10:24:01 +0200

    create form

--- zad9/script.js
+++ zad9/script.js
@@ -101,7 +101,33 @@ function showCustomers(e) {
 		}
 		$('#customers').html(output);
 	};
+
+	let output_2 = '';
+	index.openCursor().onsuccess = function (e) {
+		var cursor = e.target.result;
+		if (cursor) {
+			output_2 += "<tr id='customer_" + cursor.value.id + "'>";
+			output_2 += "<td>" + '<label><input type="radio" name="food"><a>' + cursor.value.id + "</a> </label>"+ "</td>";
+			output_2 += "<td><span class='cursor customer' contenteditable='true' data-field='name' data-id='" + cursor.value.id + "'>" + cursor.value.name + "</span></td>";
+			output_2 += "<td><span class='cursor customer' contenteditable='true' data-field='surname' data-id='" + cursor.value.id + "'>" + cursor.value.surname + "</span></td>";
+			output_2 += "<td><span class='cursor customer' contenteditable='true' data-field='email' data-id='" + cursor.value.id + "'>" + cursor.value.email + "</span></td>";
+			output_2 += "<td><span class='cursor customer' contenteditable='true' data-field='phonenumber' data-id='" + cursor.value.id + "'>" + cursor.value.phonenumber + "</span></td>";
+
+			output_2 += "<td><span class='cursor customer' contenteditable='true' data-field='address' data-id='" + cursor.value.id + "'>" + cursor.value.address + "</span></td>";
+
+			output_2 += "<td><span class='cursor customer' contenteditable='true' data-field='nipnumber' data-id='" + cursor.value.id + "'>" + cursor.value.nipnumber + "</span></td>";
+			output_2 += "<td><span class='cursor customer' contenteditable='true' data-field='idnumber' data-id='" + cursor.value.id + "'>" + cursor.value.idnumber + "</span></td>";
+
+			output_2 += "<td><span class='cursor customer' contenteditable='true' data-field='postcode' data-id='" + cursor.value.id + "'>" + cursor.value.postcode + "</span></td>";
+			output_2 += "<td><span class='cursor customer' contenteditable='true' data-field='city' data-id='" + cursor.value.id + "'>" + cursor.value.city + "</span></td>";
+			output_2 += "</tr>";
+			cursor.continue();
+		}
+		$('#customers_2').html(output_2);
+	};
 }
+
+
 
 function removeCustomer(id) {
 	var transaction = db.transaction(["customers"], "readwrite");
